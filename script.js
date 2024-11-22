cd777a1634e83a9656859e31743f9f4a8d8154e2
unknown <yokasdenis@gmail.com>
2024-11-22 04:16:05 +0300

    daylight

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["a deepTech entrepreneur","a Data Analyst","a MERN Developer", "a Farmer"],
+    strings : ["a Data Analyst","a MERN Developer", "a Farmer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
@@ -44,7 +44,7 @@ function myMenuFunction(){
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
  const sr = ScrollReveal({
         origin: 'top',
-        distance: '80px',
+        distance: '40px',
         duration: 2000,
         reset: true     
  })
@@ -69,7 +69,7 @@ sr.reveal('.top-header',{})
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
   origin: 'left',
-  distance: '80px',
+  distance: '40px',
   duration: 2000,
   reset: true
 })
@@ -80,8 +80,8 @@ srLeft.reveal('.contact-info',{delay: 100})
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
   origin: 'right',
-  distance: '80px',
-  duration: 2000,
+  distance: '40px',
+  duration: 3000,
   reset: true
 })
 
@@ -89,29 +89,3 @@ srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
 
 
-
-/* ----- CHANGE ACTIVE LINK ----- */
-
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-  const scrollY = window.scrollY;
-
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
-
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
-    }  else {
-
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-
-    }
-  })
-}
-
-window.addEventListener('scroll', scrollActive)
