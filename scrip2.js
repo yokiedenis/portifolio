6c84a9232a53aad98127c1badc0d92af586c8bf8
unknown <yokasdenis@gmail.com>
2024-11-22 08:24:59 +0300

    daylight

---
+++ scrip2.js
@@ -0,0 +1,10 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
